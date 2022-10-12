a3cf3685e41b61fd398557f3a511f5641b3b7343
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-12 23:41:28 +0530

    add header

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Components
+import Header from './components/header';
+
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        {/* Todo form */}
+        <View style={styles.list}>
+          {/* Todo list */}
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
