ac8a06b644c47f6c6338fba056ce4729ee771904
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-12 23:50:54 +0530

    add item list

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 //Components
 import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1' },
+    { text: 'Manga parikka', key: '1' },
     { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' },
+    { text: 'Pattishow', key: '3' },
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
 
   return (  
     <View style={styles.container}>
@@ -19,7 +26,12 @@ export default function App() {
       <View style={styles.content}>
         {/* Todo form */}
         <View style={styles.list}>
-          {/* Todo list */}
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
         </View>
       </View>
     </View>
@@ -30,5 +42,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  content: {
+    padding: 20,
+    flex: 1,
   },
 });
